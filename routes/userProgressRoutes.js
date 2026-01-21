480263196604c4bf989395cb6c0a985796373dc7
thorshan <myothunaing18@gmail.com>
2026-01-21 14:53:01 +0630

    FIX

--- routes/userProgressRoutes.js
+++ routes/userProgressRoutes.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
   getUserProgress,
-  markLecturePassed,
   getLatestProgress,
+  markChapterPassed,
 } from "../controllers/userProgressController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.get("/", authMiddleware, getUserProgress);
-router.post("/pass", authMiddleware, markLecturePassed);
+router.post("/pass", authMiddleware, markChapterPassed);
 router.get("/latest", authMiddleware, getLatestProgress);
 
 export default router;
